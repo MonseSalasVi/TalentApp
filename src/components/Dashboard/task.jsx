6635216149f096c39bbe68a8bfd4e3d5e6a5a462
26erika <develop.erika@gmail.com>
2020-05-14 13:17:19 -0500

    merged components

---
+++ src/components/Dashboard/task.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "@material-ui/core/Card";
+import Select from "@material-ui/core/Select";
+
+const Task = () => {
+  return (
+    <Card>
+      <h4>Nombre de la tarea</h4>
+      <Select></Select>
+    </Card>
+  );
+};
+
+export default Task;
