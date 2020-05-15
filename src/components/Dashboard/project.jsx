6108480eba742f69b84cd223b3fb9e1018453911
MonseSalasVi <cmonse.salasv@gmail.com>
2020-05-14 20:07:14 -0500

    }

--- src/components/Dashboard/project.jsx
+++ src/components/Dashboard/project.jsx
@@ -4,7 +4,7 @@ import Card from "@material-ui/core/Card";
 import Divider from "@material-ui/core/Divider";
 import { getTasks, getProjects } from "../../UseFetch";
 import Box from "@material-ui/core/Box";
-        
+
 getTasks("7").then((data) => {
   console.log(data);
 });
@@ -52,8 +52,8 @@ const Project = () => {
 
       <div>
         <Card>
-          {tasks.map((t , i) => (
-            <Task key={i} taskId={t.id />
+          {tasks.map((t, i) => (
+            <Task key={i} taskId={t.id} />
           ))}
         </Card>
       </div>
