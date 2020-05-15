e15e97486c8bc3e9c95272842bae011c9a9cb6d4
MonseSalasVi <cmonse.salasv@gmail.com>
2020-05-15 02:07:23 -0500

    Karen, login user

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,11 @@
-
-import React, {useState,useContext} from "react";
+import React, { useState, useContext } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import {UserContextProvider} from "./UserContext"
+import { UserContextProvider } from "./UserContext";
 import { makeStyles } from "@material-ui/core/styles";
 import Access from "./components/access";
 import Dashboard from "./components/Dashboard/dashboard";
 import Stats from "./components/stats";
-
 import Layout from "./components/Layout/Layout";
 
 const useStyles = makeStyles((theme) => ({
@@ -25,58 +23,15 @@ const Routes = () => {
 
   return (
     <Router>
-      <Navbar />
-      <div
-        style={{
-          display: "flex",
-        }}
-      >
-        <Drawer
-          style={{
-            width: "240px",
-          }}
-          variant="persistent"
-          anchor="left"
-          open={true}
-          classes={{
-            paper: classes.drawerPaper,
-          }}
-        >
-          <List>
-            <Link to="/dashboard" className={classes.link}>
-              <ListItem button>
-                <ListItemIcon>
-                  <CheckBoxIcon />
-                </ListItemIcon>{" "}
-                <ListItemText primary="Mis tareas" />
-              </ListItem>{" "}
-            </Link>{" "}
-            <Link to="/stats" className={classes.link}>
-              <ListItem button>
-                <ListItemIcon>
-                  <DonutLargeIcon />
-                </ListItemIcon>{" "}
-                <ListItemText primary="Estadísticas" />
-              </ListItem>{" "}
-            </Link>{" "}
-            <Link to="/access" className={classes.link}>
-              <ListItem button>
-                <ListItemIcon>
-                  <ExitToAppIcon />
-                </ListItemIcon>{" "}
-                <ListItemText primary="Salir" />
-              </ListItem>{" "}
-            </Link>{" "}
-          </List>{" "}
-        </Drawer>{" "}
-        <UserContextProvider>
-            <Switch>
-              <Route exact path="/dashboard" component={Dashboard} />{" "}
-              <Route exact path="/stats" component={Stats} />{" "}
-              <Route exact path="/access" component={Access} />{" "}
-            </Switch>{" "}
-        </UserContextProvider>
-      </div>{" "}
+      <UserContextProvider>
+        <Layout>
+          <Switch>
+            <Route exact path="/dashboard" component={Dashboard} />{" "}
+            <Route exact path="/stats" component={Stats} />{" "}
+            <Route exact path="/access" component={Access} />{" "}
+          </Switch>
+        </Layout>
+      </UserContextProvider>
     </Router>
   );
 };
